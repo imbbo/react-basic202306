4663258642720b93b0dc373c87306cab24a13ece
KJH <0311wlsgus@gmail.com>
2023-06-13 12:12:53 +0900

    useEffect

--- src/component/chap03_state/Counter.js
+++ src/component/chap03_state/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Counter = () => {
 
@@ -10,9 +10,28 @@ const Counter = () => {
 
     const changeNick = () => setNick('김철수');
 
+    /*
+        useEffect:
+    - 화면이 처음으로 렌더링(마운트) 될 때 자동으로 호출되는 함수.
+    - 상태값이 변경될 때마다 다시 호출.
+    - 만약에 첫 렌더링 시에만 useEffect를 호출하고 싶다면
+    두번쨰 파라미터로 빈 배열(의존성 배열)을 넣으면 된다.
+    - 의존성 배열에 상태변수를 넣으면 해당 상태변수가 업데이트 될 때마다 
+    useEffect가 재 호출됨
+    */
+    
+    useEffect(()=> {
+        // 화면이 처음 렌더링 될 떄 fetch를 통해 서버 api를 호출하는 경우,
+        // 처음 1회 호출 후에 또 할 필요가 있을까??
+        console.log('3. useEffect call!');
+    }, [nick]);
+
+    console.log('1. component fuction executed!');
+
   return (
 
     <>
+        {console.log('2. rendered html!')}
         <h1>닉네임: {nick}</h1>
         <h2>{number}</h2>
         <button onClick={increse}>+1</button>
